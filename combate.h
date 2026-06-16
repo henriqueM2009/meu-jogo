35c85d3d75a17a4c4fa6a9bf79effe5245e09af4
HENRIQUE MORAES GOIS <henrique_m_gois@estudante.sesisenai.org.br>
2026-06-16 15:52:53 -0300

    O modo de combate foi iniciado

---
+++ combate.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int Exibir() {
+	
+	setlocale(LC_ALL, "Português");
+	int vida, ataque;
+	
+	cout << "Seja bem-vinda, Mary, tudo bem??? ";
+	cout << "\n Digite sua quantidade de vida: ";
+	cin >> vida;
+	cout << "\n Digite sua força: ";
+	cin >> ataque;
+	cout << "\n Okk, Espero que curta o jogo! :3";
+	
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "combate.h"
 using namespace std;
 
 int main() {
+	setlocale(LC_ALL, "Português");
 	
 	cout << "Mary's World" << endl;
+	Exibir();
 	
 	return 0;
 }
